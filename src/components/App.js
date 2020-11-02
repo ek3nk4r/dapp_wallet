a9bb423b815cf04bf40249c4490ea0ddcec25444
xternet <majewski.post@gmail.com>
2020-11-02 18:44:42 +0100

    .

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,19 @@ import Navbar from './Navbar'
 import Main from './Main'
 import Web3 from 'web3';
 
-/* T0d0
-[ ] auto mm
-[x] loading
-[ ] readMe gif
-[x] navbar main
-*/
-
 class App extends Component {
 
   async componentWillMount() {
     await this.init()
   }
 
+  /** Settings for:
+    *   Web3Modal+WalletConnect 
+    *   Web3Modal+MetaMask,
+    *   MetaMask.
+  */ 
   async init() {
+    // Declare WalletConnect
     const providerOptions = {
       walletconnect: {
         package: WalletConnectProvider,
@@ -32,12 +31,12 @@ class App extends Component {
     var web3Modal = new Web3Modal({
       cacheProvider: true, // optional
       providerOptions, // required
-      disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
+      disableInjectedProvider: false, // Declare MetaMask
     });
 
     this.setState({web3Modal: web3Modal})
 
-    //Setting for MetaMask
+    //Settings for only MetaMask
     if(typeof window.ethereum!=='undefined'){
       let network, balance, web3
 
@@ -45,66 +44,65 @@ class App extends Component {
       web3 = new Web3(window.ethereum)
       this.setState({web3: web3})
 
-
+      //Update address&account when MM user change account
       window.ethereum.on('accountsChanged', async (accounts) => {
-        if(typeof accounts[0] === 'undefined') {
-          this.setState({
-            account: null,
-            balance: null
-          })
-        } else {
+        if(typeof accounts[0] === 'undefined'){
+          this.setState({ account: null, balance: null, provider: null})
+        } else if(this.state.provider === null){
           this.setState({account: null, balance: null, loading: true})
           balance = await web3.eth.getBalance(accounts[0])
-
-          this.setState({
-            account: accounts[0],
-            balance: balance,
-            loading: false
-          })
+          this.setState({account: accounts[0], balance: balance, loading: false })
         }
       });
 
       window.ethereum.on('chainChanged', async (chainId) => {
-        this.setState({network: null, balance: null, loading: true})
-        network = await getChain(parseInt(chainId, 16))
+        this.setState({network: null, balance: null, loading: true, onlyNetwork: true})
 
-        if(this.state.account!==null){
+        if(this.state.account){
           balance = await web3.eth.getBalance(this.state.account)
+          this.setState({balance: balance})
         }
 
-        this.setState({
-          network: network.network,
-          balance: balance,
-          loading: false
-        })
+        network = await getChain(parseInt(chainId, 16))
+        this.setState({ network: network.network, loading: false, onlyNetwork: false})
       });
     }
   }
 
+  /**
+    * "Connect" button, selecting provider via Web3Modal
+  */ 
   async on(event) {
     event.preventDefault()
+
+    // Restore provider session
     await this.state.web3Modal.clearCachedProvider();
-
-    try {
-      let provider, account, network, balance, web3
+    let provider, account, network, balance, web3
+    
+    try {      
+      // Activate windows with providers (MM and WC) choice
       provider = await this.state.web3Modal.connect();
-      console.log('provider: ', provider)
+      console.log('Provider: ', provider)
 
-      this.setState({
-        loading: true,
-        provider: null
-      }) 
+      this.setState({ loading: true, provider: null }) 
 
-      if(provider.isMetaMask){
+      if(provider.isMetaMask){ // When MetaMask was chosen as a provider
         account = provider.selectedAddress
         network = await getChain(parseInt(provider.chainId, 16))
         web3 = new Web3(provider)
         balance = await web3.eth.getBalance(provider.selectedAddress)
-      } else if (provider.wc){
-        account = provider.accounts[0]
-        network = await getChain(provider.chainId)
-        web3 = new Web3(new Web3.providers.HttpProvider(`https://${network.network}.infura.io/v3/db6231b5ef424bd9a61a76670e56086b`));
-        balance = await web3.eth.getBalance(account)
+      } else if (provider.wc){ // When WalletConect was chosen as a provider
+        if(provider.accounts[0]!=='undefined') {
+          account = await provider.accounts[0]
+          network = await getChain(provider.chainId)
+          web3 = new Web3(new Web3.providers.HttpProvider(`https://${network.network}.infura.io/v3/db6231b5ef424bd9a61a76670e56086b`));
+          balance = await web3.eth.getBalance(account)
+        } else { //handle problem with providing data
+          account = null
+          network = null
+          balance = null
+          web3 = new Web3(new Web3.providers.HttpProvider(`https://${network}.infura.io/v3/db6231b5ef424bd9a61a76670e56086b`));
+        }
       } else {
         window.alert('Error, provider not recognized')
       }
@@ -124,113 +122,131 @@ class App extends Component {
     }
 
     // Update account&balance
-    this.state.provider.on("accountsChanged", async (accounts) => {
+    provider.on("accountsChanged", async (accounts) => {
       let account, balance, network, web3
 
-      this.setState({
-        account: null,
-        balance: null,
-        loading: true
-      })      
+      this.setState({ account: null, balance: null, loading: true })      
 
-      if(this.state.provider.isMetaMask && this.state.provider.selectedAddress!==null){
-        web3 = new Web3(this.state.provider)
-        balance = await web3.eth.getBalance(this.state.provider.selectedAddress)
-      } else if (this.state.provider.wc){
-        account = this.state.provider.accounts[0]
-        network = await getChain(this.state.provider.chainId)
+      if(provider.isMetaMask && provider.selectedAddress!==null){
+        web3 = new Web3(provider)
+        balance = await web3.eth.getBalance(provider.selectedAddress)
+      } else if (provider.wc){
+        account = provider.accounts[0]
+        network = await getChain(provider.chainId)
         web3 = new Web3(new Web3.providers.HttpProvider(`https://${network.network}.infura.io/v3/db6231b5ef424bd9a61a76670e56086b`));
         balance = await web3.eth.getBalance(account)
       }
-      this.setState({
-        account: accounts[0],
-        balance: balance,
-        loading: false
-      })
+
+      this.setState({ account: accounts[0], balance: balance, loading: false })
     });
 
     // Update network
-    this.state.provider.on("chainChanged", async (chainId) => {
+    provider.on("chainChanged", async (chainId) => {
       let account, balance, network, web3
-      
-      this.setState({
-        loading: true,
-        network: null,
-        balance: null
-      })
-
-      if(this.state.provider.isMetaMask){
-        web3 = new Web3(this.state.provider)
-        balance = await web3.eth.getBalance(this.state.provider.selectedAddress)
-        network = await getChain(parseInt(this.state.provider.chainId, 16))
-      } else if (this.state.provider.wc){
-        account = this.state.provider.accounts[0]
+      this.setState({balance: null, network: null, loading: true })
+      if(provider.isMetaMask && provider.selectedAddress!==null){
+        web3 = new Web3(provider)
+        balance = await web3.eth.getBalance(provider.selectedAddress)
+        network = await getChain(parseInt(provider.chainId, 16))
+      } else if(provider.wc){
+        account = provider.accounts[0]
         network = await getChain(chainId)
         web3 = new Web3(new Web3.providers.HttpProvider(`https://${network.network}.infura.io/v3/db6231b5ef424bd9a61a76670e56086b`));
         balance = await web3.eth.getBalance(account)
+        this.setState({ balance: balance, network: network.network, loading: false })
+      } else if (provider.selectedAddress===null){
+        network = await getChain(parseInt(provider.chainId, 16))
+        this.setState({ network: network.network, loading: false })
       }
 
-
-      this.setState({
-        balance: balance,
-        network: network.network,
-        loading: false
-      })
+      this.setState({ balance: balance, network: network.network, loading: false })
     });
   }
 
+  /**
+    * Disconnect button
+   */
   async off(event) {
     event.preventDefault()
 
-    if(this.state.provider===null){
-      window.alert('Logout on MetaMask')
-    } else {
+    if(this.state.provider===null || typeof this.state.provider==='undefined'){
+      window.alert('Logout on MetaMask') // Inform to disconnect from MetaMask
+    }
+    else {
       if(this.state.provider!==null && this.state.provider.wc) {
-        await this.state.provider.stop() //WalletConnnect
-        // await this.state.provider.disconnect() //Off QRcode
-      } else if (this.state.provider!==null && this.state.provider.isMetaMask) {
-        await this.state.provider.close
-      } 
-      this.setState({web3: null});
-      this.setState({account: null});
-      this.setState({balance: null});
-      this.setState({network: null});
+        await this.state.provider.stop() // Disconnect Web3Modal+WalletConnnect (QR code remains)
+        this.setState({account: null, balance: null});
+
+        //In case if MetaMask is installed
+        if(window.ethereum){
+          const network = await getChain(parseInt(window.ethereum.chainId, 16))
+          this.setState({network: network.network})
+        } else {
+          this.setState({network: null})
+        }
+      } else if (this.state.provider!==null && this.state.provider.isMetaMask){
+        await this.state.provider.close // Disconnect Web3Modal+MetaMask
+      }
+      // Reset UI
       this.setState({provider: null});
 
+      // Restart provider session
       await this.state.web3Modal.clearCachedProvider();
+     }
+  }
+
+  async offQr(event) {
+    event.preventDefault()
+
+    if(this.state.provider.wc){
+      await this.state.provider.disconnect()
+      this.setState({
+        account: null,
+        balance: null,
+        provider: null
+      })
+      if(window.ethereum){
+        const network = await getChain(parseInt(window.ethereum.chainId, 16))
+        this.setState({network: network.network})
+      } else {
+        this.setState({network: null})
+      }
     }
   }
 
+  /** 
+    * "Send 1 Wei to yourself" button
+  */
   async send(event){
     event.preventDefault()
 
     if(this.state.provider===null && !this.state.account) {
       window.alert('Error')
-    } else if (this.state.provider===null && this.state.account) {
-      this.state.web3.eth.sendTransaction({from: this.state.account, to: this.state.account, value: '1'})
-    } else if(this.state.provider.isMetaMask || this.state.provider===null){
-      this.state.web3.eth.sendTransaction({from: this.state.account, to: this.state.account, value: '1'})
-    } else if (this.state.provider.wc){
-        window.alert('Accept on phone')
+    } else if (this.state.provider===null && this.state.account) { //MetaMask
+      this.state.web3.eth.sendTransaction({from: this.state.account, to: this.state.account, value: '1'}).on('error', (e) => window.alert('Error'))
+    } else if(this.state.provider.isMetaMask || this.state.provider===null){ //Web3Modal+MetaMask
+      this.state.web3.eth.sendTransaction({from: this.state.account, to: this.state.account, value: '1'}).on('error', (e) => window.alert('Error'))
+    } else if (this.state.provider.wc){  //Web3Modal+WalletConnect
+      window.alert('Accept on phone')
 
-        //Declare data for JSON RPC request
-        const from = this.state.account
-        const to = this.state.account
-        const value = 1 //1 wei
+      //Declare data for JSON RPC request
+      const from = this.state.account
+      const to = this.state.account
+      const value = 1 //wei
 
-        //request
-        const tx = {
-          "method": "eth_sendTransaction",
-          "params": [{ from, to, value }]
-        }
-
-        try {
-          await this.state.provider.request(tx)
-        } catch (error) {
-          console.log('error: ', error)
-          return;
-        }
+      //Request
+      const tx = {
+        "method": "eth_sendTransaction",
+        "params": [{ from, to, value }]
       }
+
+      try {
+        await this.state.provider.request(tx) //Send request
+      } catch (error) {
+        console.log('error: ', error)
+        return;
+      }
+    }
   }
 
   constructor(props) {
@@ -238,14 +254,16 @@ class App extends Component {
     this.state = {
       account: null,
       balance: null,
-      loading: false,
       network: null,
-      provider: null
+      provider: null,
+      loading: false,
+      onlyNetwork: false
     }
 
-   this.on = this.on.bind(this)
-   this.off = this.off.bind(this)
-   this.send = this.send.bind(this)
+    this.on = this.on.bind(this)
+    this.off = this.off.bind(this)
+    this.send = this.send.bind(this)
+    this.offQr = this.offQr.bind(this)
   }
 
   render() {
@@ -259,11 +277,13 @@ class App extends Component {
         />&nbsp;
         <Main
           send={this.send}
+          offQr={this.offQr}
           account={this.state.account}
           balance={this.state.balance}
           loading={this.state.loading}
           network={this.state.network}
           provider={this.state.provider}
+          onlyNetwork={this.state.onlyNetwork}
         />
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
-import wmwcLogo from '../logos/Web3Modal+WC.png';
-import wmmmLogo from '../logos/Web3Modal+MM.png';
-import mmLogo from '../logos/MetaMask.png';
 import React, { Component } from 'react';
 import Identicon from 'react-identicons';
-import none from '../logos/none.png';
+import wmwc from '../logos/wmwc.png';
+import wmmm from '../logos/wmmm.png';
+import all from '../logos/all.png';
 import eth from '../logos/eth.png';
+import mm from '../logos/mm.png';
 import './App.css';
 
 class Main extends Component {
@@ -13,16 +13,12 @@ class Main extends Component {
     return (
       <div className="container-fluid mt-5 text-center">
         <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ width: '750px' }}>
-          <a
-              href="https://github.com/xternet/dapp_wallet"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            { this.props.provider===null && this.props.account===null
+          <span>
+            { this.props.provider===null && !this.props.account
               ? <figure>
-                  <img src={none} className="App-logo" alt="logo" width='256px'/>
+                  <img src={all} className="App-logo" alt="logo" width='256px'/>
                   <figcaption
-                    style={{ width: '512px', fontSize: '14px'}}
+                    style={{ width: '512px', fontSize: '15px'}}
                     className="text-right text-monospace text-dark">
                     Connection:<br></br>
                     <b>None</b>
@@ -30,9 +26,9 @@ class Main extends Component {
                 </figure>
               : this.props.provider===null && this.props.account!==null
                 ? <figure>
-                    <img src={mmLogo} className="App-logo" alt="logo" width='256px'/>
+                    <img src={mm} className="App-logo" alt="logo" width='256px'/>
                     <figcaption
-                      style={{ width: '512px', fontSize: '14px'}}
+                      style={{ width: '512px', fontSize: '15px'}}
                       className="text-right text-monospace text-dark">
                       Connection:<br></br>
                       <b>MetaMask</b>
@@ -40,25 +36,40 @@ class Main extends Component {
                   </figure>
                 :  this.props.provider.isMetaMask
                      ?  <figure>
-                          <img src={wmmmLogo} className="App-logo" alt="logo" width='256px'/>
+                          <img src={wmmm} className="App-logo" alt="logo" width='256px'/>
                           <figcaption
-                            style={{ width: '512px', fontSize: '14px'}}
+                            style={{ width: '512px', fontSize: '15px'}}
                             className="text-right text-monospace text-dark">
                             Connection:<br></br>
                             <b>Web3Modal+MetaMask</b>
                           </figcaption>
                         </figure>
-                     :  <figure>
-                          <img src={wmwcLogo} className="App-logo" alt="logo" width='256px'/>
-                          <figcaption
-                            style={{ width: '512px', fontSize: '14px'}}
-                            className="text-right text-monospace text-dark">
-                            Connection:<br></br>
-                            <b>Web3Modal+WalletConnect</b>
-                          </figcaption>
-                        </figure>
+                     :  <div>
+                          <figure>
+                            <img src={wmwc} className="App-logo" alt="logo" width='256px'/>
+                            <figcaption
+                              style={{ width: '512px', fontSize: '15px'}}
+                              className="text-right text-monospace text-dark">
+                              Connection:<br></br>
+                              <div className="row">
+                                <div className="col">
+                                  <span
+                                    type="submit"
+                                    onClick={(e) => this.props.offQr(e)}
+                                    className="text-danger bg-dark"
+                                    style={{ height: '15px', fontSize: '15px'}}
+                                    ><b>Disconnect&off Qr Code</b>
+                                  </span>
+                                </div>
+                                <div className="col">
+                                  <b>Web3Modal+WalletConnect</b>
+                                </div>
+                              </div>
+                            </figcaption>
+                          </figure>
+                        </div>
             }
-          </a>
+          </span>
           <p></p>&nbsp;
           <table className="table-sm table-bordered border-dark text-monospace table-responsive overflow-hidden" style={{ width: '750px', height: '74px' }}>
             <thead>
@@ -68,50 +79,58 @@ class Main extends Component {
                 <th>Network</th>
               </tr>
               <tr>
-                {  !  this.props.account && ! this.props.loading
+                {  !  this.props.account && ! this.props.loading //Update Account
                    ?  <td style={{ width: '500px' }}>
                         ---
                       </td>
-                   :  ! this.props.account && this.props.loading
+                   :  ! this.props.account && this.props.loading  && ! this.props.onlyNetwork
                         ? <td style={{ width: '500px'}}>
                             <div className="spinner-border spinner-border-sm text-primary" role="status">
                               <span className="sr-only">Loading...</span>
                             </div>
                           </td>
-                        : <td style={{ width: '500px'}}>
-                            <Identicon string={this.props.account} size="20" className='align-text-bottom' bg='#343A40'/>&nbsp;
-                            { this.props.network==='mainnet'
-                              ? <a
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  href={"https://etherscan.io/address/" + this.props.account}>
-                                  <b>{this.props.account}</b>
-                                </a>
-                              : <a
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  href={`https://${this.props.network+'.'}etherscan.io/address/`+this.props.account}>
-                                  <b>{this.props.account}</b>
-                                </a>
-                            }
-                          </td>
-                }{ !  this.props.balance && ! this.props.loading
+                        : ! this.props.account && this.props.loading  && this.props.onlyNetwork
+                            ?  <td style={{ width: '500px' }}>
+                                ---
+                               </td> 
+                            :  <td style={{ width: '500px'}}>
+                                  <Identicon string={this.props.account} size="20" className='align-text-bottom' bg='#343A40'/>&nbsp;
+                                  { this.props.network==='mainnet'
+                                    ? <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href={"https://etherscan.io/address/" + this.props.account}>
+                                        <b>{this.props.account}</b>
+                                      </a>
+                                    : <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href={`https://${this.props.network+'.'}etherscan.io/address/`+this.props.account}>
+                                        <b>{this.props.account}</b>
+                                      </a>
+                                  }
+                                </td>
+                }{ !  this.props.balance && ! this.props.loading //Update Balance
                    ?  <td style={{ width: '125px'}}>
                         ---
                       </td>
-                   :  ! this.props.balance && this.props.loading
+                   :  ! this.props.balance && this.props.loading && ! this.props.onlyNetwork
                         ? <td style={{ width: '125px'}}>
                             <div className="spinner-border spinner-border-sm text-primary" role="status"></div>
                           </td>
-                        : <td style={{ width: '125px'}}>
-                            <b>{(this.props.balance/10**18).toFixed(5)}</b>
-                            <img src={eth} className="align-text-bottom" height="20" alt="id" />
-                          </td>
-                }{ !  this.props.network && ! this.props.loading
+                        : ! this.props.balance && this.props.loading && this.props.onlyNetwork
+                          ?  <td style={{ width: '125px'}}>
+                              ---
+                             </td>
+                          : <td style={{ width: '125px' }}>
+                              <b>{(this.props.balance/10**18).toFixed(5)}</b>
+                              <img src={eth} className="align-text-bottom" height="20" alt="id" />
+                            </td>
+                }{ !  this.props.network && ! this.props.loading && ! this.props.onlyNetwork //Update Network
                    ?  <td style={{ width: '125px'}}>
                         ---
                       </td>
-                   :  ! this.props.network && this.props.loading
+                   :  ! this.props.network && this.props.loading && this.props.onlyNetwork
                         ? <td style={{ width: '125px'}}>
                             <div className="spinner-border spinner-border-sm text-primary" role="status"></div>
                           </td>
@@ -123,9 +142,9 @@ class Main extends Component {
             </thead>
           </table>
             &nbsp;
-            { !  this.props.account && ! this.props.loading 
+            { !  this.props.account && ! this.props.loading //Update "Send 1 Wei to yourself" Button
               ?  <div className="row text">&nbsp;&nbsp;</div>
-              :  !  this.props.account && this.props.loading
+              :  !  this.props.account && this.props.loading && ! this.props.onlyNetwork
                  ?  <div className="row text-center text-monospace">
                       <button
                         type="button"
@@ -137,15 +156,18 @@ class Main extends Component {
                         <span className="sr-only">Loading...</span>
                       </button>
                     </div>
-                 :  <div className="row text-center text-monospace">
-                      <button
-                        type="submit"
-                        onClick={(e) => this.props.send(e)}
-                        className="btn btn-primary btn-sm mx-auto"
-                        style={{ width: '375px', fontSize: '17px' }}
-                        ><b>Send 1 wei to yourself</b>
-                      </button>
-                    </div>
+                 :  ! this.props.account && this.props.loading && this.props.onlyNetwork
+                    ? <div className="row text">&nbsp;&nbsp;</div>
+                    : <div className="row text-center text-monospace">
+                        <button
+                          type="submit"
+                          onClick={(e) => this.props.send(e)}
+                          className="btn btn-primary btn-sm mx-auto"
+                          style={{ width: '375px', fontSize: '17px' }}
+                          ><b>Send 1 wei to yourself</b>
+                        </button>
+                      </div>
+
             }
         </main>
       </div>
